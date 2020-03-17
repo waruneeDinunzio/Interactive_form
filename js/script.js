17451024a6f1e0372a1122fa7538c1727bc55a92
waruneeDinunzio <56564699+waruneeDinunzio@users.noreply.github.com>
2020-03-17 13:21:05 -0400

    finised 1st part and add js/form_validation.js
    finised all section and working on form validation

---
+++ js/script.js
@@ -0,0 +1,142 @@
+window.addEventListener('DOMContentLoaded', (event) => { 
+    const nameFocus = document.getElementById('name').focus();
+    const otherTitle = document.getElementById('other-title');
+    //hide Job role: other
+    otherTitle.style.display = 'none';
+
+    const otherOption = document.getElementById('title');
+    otherOption.addEventListener("change", () => {
+        if (otherOption.value==="other"){
+        otherTitle.style.display = ''; 
+        }else{
+        otherTitle.style.display ='none'; 
+        }
+    });
+
+    /*
+    T-Shirt Info section
+    */
+    const size = document.getElementById('size');
+    const design = document.getElementById('design');
+    const color = document.getElementById('color');
+    const colorOption = document.querySelectorAll('#color option')
+    //console.log(colorOption);
+    //console.log(design);
+    let designSelect = document.querySelectorAll('#design option');
+        //Hide the "Select Theme" option
+        //console.log(designSelect[0]);
+        designSelect[0].hidden = true;
+        colorOption[0].innerHTML = "Please select a T-shirt theme";
+        //console.log(colorOption[0]);
+
+    for (let i = 0; i < colorOption.length; i +=1) {
+    color[i].style.display = 'none';
+    } 
+
+    //console.log(colorOption);
+    design.addEventListener('change', (e) => {
+       const eventTheme = e.target.value;
+       console.log(designSelect[1].value);
+       console.log(eventTheme);
+       if (designSelect[1].value === eventTheme) {
+            color[0].innerHTML = 'Cornflower Blue (JS Puns shirt only)';
+            color[0].selected = true;
+            color[0].style.display = 'block';
+            color[1].style.display = 'block';
+            color[2].style.display = 'block';
+            color[3].style.display = 'none';
+            color[4].style.display = 'none';
+            color[5].style.display = 'none';  
+       } else {
+            color[0].style.display = 'none';
+            color[1].style.display = 'none';
+            color[2].style.display = 'none';
+            color[3].selected = true;
+            color[3].style.display = 'block';
+            color[4].style.display = 'block';
+            color[5].style.display = 'block';  
+       }
+        
+    });
+    /*
+    Activity section
+    */
+   //crate element to display the total activity cost
+   let totalActivitiesCost = 0;
+   let totalCost = document.createElement('h2');
+   totalCost.innerHTML = "Total cost: " + totalActivitiesCost + " $";
+   const activity = document.querySelector('.activities');
+   //console.log(activity);
+   activity.appendChild(totalCost);
+   const activityInput = document.querySelectorAll('.activities input');
+        //console.log(activityInput);
+    //crate event listener when user selected the activity
+    activity.addEventListener('change', (e) => {
+       const activityTarget = e.target;
+       console.log(activityTarget);
+       const activityCost = parseInt(activityTarget.getAttribute('data-cost'));
+        console.log(activityCost);
+        if (activityTarget.checked) {
+            totalActivitiesCost= totalActivitiesCost+activityCost;
+            console.log(totalActivitiesCost);
+            totalCost.innerHTML = "Total cost: " + totalActivitiesCost + " $"
+        } else {
+            totalActivitiesCost= totalActivitiesCost-activityCost;
+            totalCost.innerHTML = "Total cost: " + totalActivitiesCost + " $"
+        }
+        const activityDayTime = activityTarget.getAttribute('data-day-and-time');
+        console.log(activityDayTime);
+        //const activityInput = activity.input;
+        //console.log(activityInput);
+        for (let i = 0; i<activityInput.length; i +=1) {
+            let checkInput = activityInput[i].getAttribute('data-day-and-time');
+            if (activityDayTime === checkInput && activityTarget !== activityInput[i]) {
+                if (activityTarget.checked) {
+                    activityInput[i].disabled = true;
+                } else {
+                    activityInput[i].disabled = false;
+                }
+            }
+        }
+    });
+
+    /*
+    Payment Info section
+    */
+   const payment = document.getElementById('payment');
+   let paymentSelect = document.querySelectorAll('#payment option');
+   //Hide the "Select Payment Method" option
+   console.log(paymentSelect);
+   paymentSelect[0].hidden = true;
+   paymentSelect[0].disabled = true;
+    const creditCard = document.getElementById('credit-card');
+    const paypal = document.getElementById('paypal');
+    const bitcoin = document.getElementById('bitcoin');
+    console.log(creditCard);
+    console.log(paypal);
+    console.log(payment);
+    paypal.style.display = 'none';
+    bitcoin.style.display = 'none';
+    payment.addEventListener('change', (e) => {
+        let paymentTarget = e.target.value;
+        console.log(paymentTarget);
+        console.log(paymentSelect[1].value);
+    for (let i = 0; i < paymentSelect.length; i +=1) {
+        if (paymentTarget=== 'credit card') {
+            creditCard.style.display = '';
+            paypal.style.display = 'none';
+            bitcoin.style.display = 'none';
+        } else if (paymentTarget=== 'paypal') {
+            creditCard.style.display = 'none';
+            paypal.style.display = '';
+            bitcoin.style.display = 'none';   
+        } else {
+            creditCard.style.display = 'none';
+            paypal.style.display = 'none';
+            bitcoin.style.display = '';
+        }
+    }
+
+    });
+
+});
